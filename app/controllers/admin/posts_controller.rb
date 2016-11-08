6dd8e11c38e676db9193f4c81883fa71b34ce07b
Liang-Hsuan Ma <leonma333@hotmail.com>
2016-11-07 22:30:45 -0500

    adding admin post new page

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,5 +3,10 @@ class Admin::PostsController < ApplicationController
 	def index
 		@posts = Post.all.order(:created_at, :DESC)
 	end
+
+	def new
+		@post = Post.new
+		@image_list = Dir.glob("#{Rails.root}/app/assets/images/post/*").map{|path| path.split('/').last}
+	end
 	
 end
