67d69cbde36cc860da14116c541f144f0f25e2ee
Liang-Hsuan Ma <leonma333@hotmail.com>
2016-11-08 22:41:10 -0500

    complete adding post and admin layout functionality

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,12 +1,30 @@
-class Admin::PostsController < ApplicationController
+class Admin::PostsController < Admin::AdminController
 
 	def index
 		@posts = Post.all.order(:created_at, :DESC)
+		@page_title = "Kicks4Love Admin | Posts"
 	end
 
 	def new
 		@post = Post.new
 		@image_list = Dir.glob("#{Rails.root}/app/assets/images/post/*").map{|path| path.split('/').last}
+		@page_title = "Kicks4Love Admin | New Post"
+	end
+
+	def create
+		post = Post.new post_params
+
+		if post.save
+			redirect_to admin_posts_path, :notice => "New post successfully created"
+		else
+			redirect_to :back, :error => "Error creating new post"
+		end
+	end
+
+	private 
+
+	def post_params
+		params.require(:post).permit(:title, :content, :image)
 	end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   	resources :main
 
+  	get '/admin/', :to => 'admin/posts#index', :as => :admin_root
+
   	namespace :admin do
   		resources :posts
   	end
