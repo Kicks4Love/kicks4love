b2b32a07ecd3178dac6becfc13a6417cc6116894
Liang-Hsuan Ma <leonma333@hotmail.com>
2016-11-09 22:49:46 -0500

    finish part of home page admin

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,10 @@
 class Admin::PostsController < Admin::AdminController
 
+	skip_before_filter :verify_authenticity_token, :only => [:destroy]
+	before_action :get_post, :only => [:edit, :destroy, :update, :show]
+
 	def index
-		@posts = Post.all.order(:created_at, :DESC)
+		@posts = Post.all.order(:created_at => :DESC)
 		@page_title = "Kicks4Love Admin | Posts"
 	end
 
@@ -21,10 +24,39 @@ class Admin::PostsController < Admin::AdminController
 		end
 	end
 
+	def update
+		if @post.update_attributes(post_params)
+			flash[:notice] = "The post has been successfully updated"
+		else
+			flash[:error] = "Error occurs while updating the post, please try again"
+		end
+
+		redirect_to admin_posts_path
+	end
+
+	def edit
+		@image_list = Dir.glob("#{Rails.root}/app/assets/images/post/*").map{|path| path.split('/').last}
+		@page_title = "Kicks4Love Admin | New Post"
+	end
+
+	def destroy
+		if @post.destroy
+			flash[:notice] = "The post has been deleted successfully"
+		else
+			flash[:error] = "Error occurs while deleting the post, please try again"
+		end
+
+		redirect_to admin_posts_path
+	end
+
 	private 
 
 	def post_params
 		params.require(:post).permit(:title, :content, :image)
+	end
+
+	def get_post
+		@post = Post.find_by_id(params[:id])
 	end
 	
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
 
 	def index
+		@post = Post.all.order(:created_at => :DESC)
 	end
 
 end
