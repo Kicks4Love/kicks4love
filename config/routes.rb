3eaaba2279817ce945ebfa52185fc01433c2efaf
Daniel Zhou <danzhou1810@gmail.com>
2016-12-04 18:10:20 -0500

    created basic on court posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
 Rails.application.routes.draw do
-  	
+
+  # namespace :admin do
+  #   get 'on_court_posts/index'
+  # end
+  #
+  # namespace :admin do
+  #   get 'on_court_posts/show'
+  # end
+  #
+  # namespace :admin do
+  #   get 'on_court_posts/new'
+  # end
+  #
+  # namespace :admin do
+  #   get 'on_court_posts/edit'
+  # end
+
   	root :to => 'main#index'
 
     get '/features' => 'main#features'
 
   	resources :main do
-  		collection do 
+  		collection do
   			get 'get_posts'
   		end
   	end
@@ -15,6 +31,7 @@ Rails.application.routes.draw do
   	namespace :admin do
   		resources :posts
       resources :feature_posts
+      resources :on_court_posts
   	end
 
 end
