3304e2ed9f8973767853d591d6f898447df6978e
Liang-Hsuan Ma <leonma333@hotmail.com>
2016-12-17 15:57:42 -0500

    more devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,29 @@
 Rails.application.routes.draw do
 
-  devise_for :admin_users
-  	root :to => 'main#index'
+  root :to => 'main#index'
 
-    get '/features' => 'main#features'
-    get '/oncourt' => 'main#oncourt'
+  get '/features' => 'main#features'
+  get '/oncourt' => 'main#oncourt'
 
-  	resources :main do
-  		collection do
-  			get 'get_posts'
-  		end
+  resources :main do
+  	collection do
+  		get 'get_posts'
   	end
+  end
 
-  	get '/admin', :to => 'admin/dashboard#index', :as => :admin_root
+  get '/admin', :to => 'admin/dashboard#index', :as => :admin_root
 
-    devise_for :admin_users, :controllers => {:sessions => 'admin/sessions'}, :skip => [:sessions]
-    devise_scope :admin_user do
-      get 'admin/login' => 'admin/sessions#new'
-      post 'admin/login' => 'admin/sessions#create'
-      delete 'admin/logout' => 'admin/sessions#destroy'
-    end
+  devise_for :admin_users, :controllers => {:sessions => 'admin/sessions'}, :skip => [:sessions]
+  devise_scope :admin_user do
+    get 'admin/login' => 'admin/sessions#new', :as => :new_admin_user_session
+    post 'admin/login' => 'admin/sessions#create', :as => :admin_user_session
+    get 'admin/logout' => 'admin/sessions#destroy', :as => :destroy_admin_user_session
+  end
 
-  	namespace :admin do
-  		resources :posts
-      resources :feature_posts
-      resources :on_court_posts
-  	end
+  namespace :admin do
+  	resources :posts
+    resources :feature_posts
+    resources :on_court_posts
+  end
 
 end
