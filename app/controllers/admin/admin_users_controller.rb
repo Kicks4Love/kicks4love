8b3e161fc613e6a7e1713ecf460f43a416666330
Liang-Hsuan Ma <leonma333@hotmail.com>
2016-12-18 16:04:37 -0500

    more things adding to admin user pages

--- app/controllers/admin/admin_users_controller.rb
+++ app/controllers/admin/admin_users_controller.rb
@@ -1,11 +1,19 @@
 class Admin::AdminUsersController < Admin::AdminController
 
+	before_action :get_admin_user, :only => [:show]
+
 	def index
 		@root_user = AdminUser.root_user
 		@admin_users = AdminUser.non_root_users
 	end
 
 	def show
+	end
+
+	private
+
+	def get_admin_user
+		@admin_user = AdminUser.find_by_id(params[:id])
 	end
 
 end
