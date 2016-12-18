67419a2fe548ed5c3ae92a0e396804b1d7efba2e
Liang-Hsuan Ma <leonma333@hotmail.com>
2016-12-18 12:05:12 -0500

    more admin user registrations stuff

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -3,4 +3,20 @@ class Admin::RegistrationsController < Devise::RegistrationsController
 	layout 'admin'
 	prepend_before_filter :require_no_authentication, :only => []
 
+	def new
+		super do |resource|
+	    	unless admin_user_signed_in?
+	    		redirect_to new_admin_user_session_path, :alert => 'You need to login before continue' and return
+	    	end
+	    	unless current_admin_user.root_user?
+	    		redirect_to admin_root_path, :alert => 'Only root user can register a new admin user' and return
+	    	end
+	    end
+	end
+
+	def create
+		super do |resource|
+		end
+	end
+
 end
