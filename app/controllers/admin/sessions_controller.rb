ad3c9c1429c35ad322eeba3176f36e5787913622
Liang-Hsuan Ma <leonma333@hotmail.com>
2016-12-18 16:04:37 -0500

    more things adding to admin user pages

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,5 +2,15 @@ class Admin::SessionsController < Devise::SessionsController
 
   layout 'admin'
   skip_before_filter :verify_authenticity_token, :only => [:destroy]
+
+  protected
+
+  def after_sign_in_path_for(resource_or_scope)
+  	admin_root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+  	new_admin_user_session_path
+  end
   
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
+	
   	protect_from_forgery with: :exception
-
-  	def after_sign_out_path_for(resource_or_scope)
-  		new_admin_user_session_path
-	end
 
 end
