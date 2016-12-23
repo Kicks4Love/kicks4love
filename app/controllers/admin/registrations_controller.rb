cb0047f9e8d9a2b1c6291e293ff84972b5080ebe
Liang-Hsuan Ma <leonma333@hotmail.com>
2016-12-22 21:16:59 -0500

    more kicks4love admin look and feel and registrations controller bug fix

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -25,25 +25,4 @@ class Admin::RegistrationsController < Devise::RegistrationsController
       admin_admin_user_path(resource)
     end
 
-	def new
-		super do |resource|
-	    	unless admin_user_signed_in?
-	    		redirect_to new_admin_user_session_path, :alert => 'You need to login before continue' and return
-	    	end
-	    	unless current_admin_user.root_user?
-	    		redirect_to admin_root_path, :alert => 'Only root user can register a new admin user' and return
-	    	end
-	    end
-	end
-
-	protected
-
-	def after_sign_up_path_for(resource)
-  		admin_admin_user_path(resource)
-	end
-
-	def after_update_path_for(resource)
-      admin_admin_user_path(resource)
-    end
-
 end
