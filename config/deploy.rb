22f0fadd40f1ec1f7bd3c7c75a0b2a3603869bb7
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-02-19 18:31:13 -0500

    add link files to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,10 +22,10 @@ set :deploy_to, "/alidata/www/kicks4love"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
