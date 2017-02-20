fe97db2ebdf3a29da65ec926a453f89fcc7fab5e
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-02-19 21:08:47 -0500

    fix syntax error (missing a comma)

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "https://github.com/Kicks4Love/kicks4love.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch "deploy"
+set :branch, "deploy"
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/alidata/www/kicks4love"
