763f1489ca587e15e80ad8a28c599de23f321a62
ZujuSun <sunzuju@gmail.com>
2017-05-02 15:53:45 -0400

    first commit

--- app/models/trend_post.rb
+++ app/models/trend_post.rb
@@ -1,7 +1,9 @@
 class TrendPost < ApplicationRecord
 
-    scope :latest, -> {order("created_at DESC")}
+    scope :latest, -> {order( :created_at => :DESC)}
 
     self.per_page = 3
-
+    
+	mount_uploader :main_image, FeatureUploader
+	mount_uploader :cover_image, FeatureUploader
   end
