01e14a754d3152c073b423df1ecf0fe6f39e8bed
Daniel Zhou <danzhou1810@gmail.com>
2017-05-03 14:39:50 -0400

    removed unnecessary uploaders

--- app/models/trend_post.rb
+++ app/models/trend_post.rb
@@ -3,7 +3,7 @@ class TrendPost < ApplicationRecord
     scope :latest, -> {order( :created_at => :DESC)}
 
     self.per_page = 3
-    
-	mount_uploader :main_image, FeatureUploader
-	mount_uploader :cover_image, FeatureUploader
+
+	mount_uploader :main_image, ImageUploader
+	mount_uploader :cover_image, ImageUploader
   end
--- app/uploaders/trend_uploader.rb
+++
@@ -1,7 +0,0 @@
-class TrendUploader < ImageUploader
-
-  version :main do
-    process resize_to_limit: [300, 220]
-  end
-
-end
