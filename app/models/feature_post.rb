3e376b6192ae9ea549fb8adb92b6602f386ff026
Daniel Zhou <danzhou1810@gmail.com>
2017-05-09 11:11:08 -0400

    Created scope "old" for ease of use

--- app/models/feature_post.rb
+++ app/models/feature_post.rb
@@ -2,11 +2,13 @@ class FeaturePost < ApplicationRecord
 
 	attr_accessor :post_type
 
-  	scope :latest, -> {order(:created_at => :DESC)}
+  scope :latest, -> {order(:created_at => :DESC)}
+
+	scope :old, -> {where("created_at < ?", 3.month.ago)}	
 
 	self.per_page = 3
 
 	mount_uploader :main_image, ImageUploader
 	mount_uploader :cover_image, ImageUploader
-	
+
 end
--- app/models/on_court_post.rb
+++ app/models/on_court_post.rb
@@ -4,6 +4,8 @@ class OnCourtPost < ApplicationRecord
 
 	scope :latest, -> {order("created_at DESC")}
 
+	scope :old, -> {where("created_at < ?", 3.month.ago)}	
+
 	self.per_page = 3
 
 	mount_uploader :cover_image, ImageUploader
