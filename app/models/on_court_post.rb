039b91325cf066cab7d0fe28556fd53e71db1ac6
Daniel Zhou <danzhou1810@gmail.com>
2017-05-09 23:58:13 -0400

    Made changes in admin models views and controllers to suit the migration

--- app/models/on_court_post.rb
+++ app/models/on_court_post.rb
@@ -3,11 +3,12 @@ class OnCourtPost < ApplicationRecord
 	attr_accessor :post_type
 
 	scope :latest, -> {order("created_at DESC")}
-	scope :old, -> {where("created_at < ?", 3.month.ago)}	
+	scope :old, -> {where("created_at < ?", 3.month.ago)}
 
 	self.per_page = 3
 
 	mount_uploader :cover_image, ImageUploader
-	mount_uploader :main_image, ImageUploader
+	mount_uploader :main_image_1, ImageUploader
+	mount_uploader :main_image_2, ImageUploader
 
 end
