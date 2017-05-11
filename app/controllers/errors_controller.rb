77d5bae073583923a14231212feb32b5214c4a7e
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-05-10 20:53:40 -0400

    adding translation to error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,11 @@
 class ErrorsController < ApplicationController
   	def not_found
+  		I18n.locale = session[:language] || I18n.default_locale
   		render(:status => 404)
   	end
 
   	def internal_server_error
+  		I18n.locale = session[:language] || I18n.default_locale
   		render(:status => 500)
   	end
 end
