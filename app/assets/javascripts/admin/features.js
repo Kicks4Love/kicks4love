ee68e2d1708560ed394b0eefeac14c2eeac32852
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-05-17 11:13:16 -0400

    make front end post composition data available

--- app/assets/javascripts/admin/features.js
+++ app/assets/javascripts/admin/features.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   initOldPostRemoval();
   initPostComposition();
   initTooltip();
+  initFormSubmit();
 });
 
 function initOldPostRemoval() {
@@ -30,20 +31,25 @@ function initOldPostRemoval() {
   });
 }
 
+/* function that allow user to compose the post's images and contents order */
 function initPostComposition() {
   var enCount = cnCount = 0;
   var displayParagraph = $('#number-paragraph');
   var displayImage = $('#number-image');
   var imageCount = parseInt($('#number-image').text());
+  var postComposition = $('#post-composition');
+  var table = postComposition.find('table');
 
   $('#feature_post_content_en').keyup(function() {
     enCount = $(this).val().split(/\r|\n/).length;
     displayParagraph.text(enCount + ' / ' + cnCount);
+    updateCompositionTable();
   });
 
   $('#feature_post_content_cn').keyup(function() {
     cnCount = $(this).val().split(/\r|\n/).length;
     displayParagraph.text(enCount + ' / ' + cnCount);
+    updateCompositionTable();
   });
 
   $('#feature_post_content_en').add('#feature_post_content_cn').keyup();
@@ -51,11 +57,52 @@ function initPostComposition() {
   $('#feature_post_main_images').change(function() {
     imageCount = $(this).get(0).files.length;
     displayImage.text(imageCount);
+    $('.main-images').remove();
+    for (var i = 0; i < imageCount; i++)
+      $('<input>').attr({
+        type: 'hidden',
+        class: 'main-images',
+        value: $(this).get(0).files[i].name
+      }).appendTo(postComposition);
+    updateCompositionTable();
   });
 
   function updateCompositionTable() {
+    var total = enCount + imageCount;
+    var selectString = '<select>';
 
+    for (var i = 1; i <= total; i++)
+      selectString += '<option value="' + i + '">' + i + '</option>';
+    selectString += '</select>';
+
+    table.empty();
+
+    $('.main-images').each(function(index) {
+      table.append('<tr data-type="image"><td>' + $(this).val() + '</td><td>' + selectString + '</td></tr>');
+    });
+
+    for (var i = 1; i <= enCount; i++)
+      table.append('<tr data-type="paragraph"><td>Paragraph ' + i + '</td><td>' + selectString + '</td></tr>')
   }
+}
+
+function initFormSubmit() {
+  $('.new_feature_post, .edit_feature_post').submit(function(event) {
+    event.preventDefault();
+
+    var postCompositionData = {};
+    var table = document.querySelector('#post-composition table');
+    for (var i = 0; i < table.rows.length; i++) {
+      var row = table.rows[i];
+      postCompositionData[$(row.cells[1]).find('select').find(':selected').val()] = {
+        type: $(row).data('type')
+      }
+    }
+
+    console.log(postCompositionData);
+
+    return false;
+  });
 }
 
 function initTooltip() {
