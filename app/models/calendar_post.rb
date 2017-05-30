6d77bbe1af49dd5cca74eee939a2c1f29315d661
ZujuSun <sunzuju@gmail.com>
2017-05-30 15:19:58 -0400

    minor fixes

--- app/models/calendar_post.rb
+++ app/models/calendar_post.rb
@@ -8,7 +8,6 @@ class CalendarPost < ApplicationRecord
 	enum :release_type => [:SNEAKER, :CLOTH, :ACCESSORY, :OTHER]
 
 	scope :latest, -> {order(:created_at => :DESC)}
-    scope :order_by_release_date, ->{order(:release_date => :DESC)}
 	scope :old, -> {where("release_date < ?", 1.month.ago)}
 
 	mount_uploader :cover_image, ImageUploader
