5e36ff5cedc8d8f787229b5b3684dae4343d9931
Daniel Zhou <danzhou1810@gmail.com>
2017-06-02 15:43:46 -0400

    better post comp refresh mechanism; larger editing space

--- app/assets/javascripts/admin/features.js
+++ app/assets/javascripts/admin/features.js
@@ -43,29 +43,33 @@ function initPostComposition() {
 
   $('#feature_post_content_en').keyup(function() {
     enCount = $(this).val().split(/\r|\n/).length;
+    currentEnCount = parseInt(displayParagraph.text().split('/')[0]);
     displayParagraph.text(enCount + ' / ' + cnCount);
-    updateCompositionTable();
+    if (currentEnCount != enCount) {
+      updateCompositionTable();
+    }
   });
 
   $('#feature_post_content_cn').keyup(function() {
     cnCount = $(this).val().split(/\r|\n/).length;
     displayParagraph.text(enCount + ' / ' + cnCount);
-    updateCompositionTable();
   });
 
   $('#feature_post_content_en').add('#feature_post_content_cn').keyup();
 
   $('#feature_post_main_images').change(function() {
     imageCount = $(this).get(0).files.length;
-    displayImage.text(imageCount);
-    $('.main-images').remove();
-    for (var i = 0; i < imageCount; i++)
-      $('<input>').attr({
-        type: 'hidden',
-        class: 'main-images',
-        value: $(this).get(0).files[i].name
-      }).appendTo(postComposition);
-    updateCompositionTable();
+    if (parseInt(displayImage.text()) != imageCount) {
+      displayImage.text(imageCount);
+      $('.main-images').remove();
+      for (var i = 0; i < imageCount; i++)
+        $('<input>').attr({
+          type: 'hidden',
+          class: 'main-images',
+          value: $(this).get(0).files[i].name
+        }).appendTo(postComposition);
+      updateCompositionTable();
+    }
   });
 
   var imageRows = table.find('tr[data-type="image"]');
