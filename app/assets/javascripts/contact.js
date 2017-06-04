20f6395b9b7968e65ac2289cfb405333194be6e9
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-06-03 20:32:13 -0400

    add about us under contact form

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
     if (getSourcePage() !== 'contact') return;
 	console.log("contact");
 
+	$('.collapse').on('show.bs.collapse', function() {
+	  	var otherPanels = $(this).parents('.card').siblings('.card');
+    	$('.collapse', otherPanels).collapse('hide');
+	});
+
 	initApplication(false, true);
 });
