a8757b4680234fd1497a2f54122e93af264b7ee6
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-06-06 09:55:24 -0400

    fix cn in mailer which should be zh

--- app/mailers/customer_service_mailer.rb
+++ app/mailers/customer_service_mailer.rb
@@ -3,7 +3,7 @@ class CustomerServiceMailer < ApplicationMailer
 	# options include first_name, last_name, chinese and comment
 	def send_contact_email(email, options={})
 	  	@contact_information = {:email => email}.merge(options)
-	  	I18n.locale = options[:chinese] ? :cn : :en
+	  	I18n.locale = options[:chinese] ? :zh : :en
 	  	mail(:to => email, :subject => I18n.t("contact_us_email_subject"), :bcc => [Rails.application.config.email_list[:leon], Rails.application.config.email_list[:robin]])
 	end
 
