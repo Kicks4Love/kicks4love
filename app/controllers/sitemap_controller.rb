d39c1509ebd7561e676663433f00c41da0337b34
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-06-23 15:18:30 -0400

    little code formatting

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -4,12 +4,14 @@ class SitemapController < ApplicationController
 
   def index
     headers['Content-Type'] = 'application/xml'
-    all_posts = { feature_posts: FeaturePost.all,
-                  oncourt_posts: OnCourtPost.all,
-                  calendar_posts: CalendarPost.all,
-                  streetsnap_posts: StreetSnapPost.all,
-                  rumor_posts: RumorPost.all,
-                  trend_posts: TrendPost.all }
+    all_posts = { 
+      feature_posts: FeaturePost.all,
+      oncourt_posts: OnCourtPost.all,
+      calendar_posts: CalendarPost.all,
+      streetsnap_posts: StreetSnapPost.all,
+      rumor_posts: RumorPost.all,
+      trend_posts: TrendPost.all 
+    }
     respond_to do |format|
         format.xml { @all_posts = all_posts }
     end
