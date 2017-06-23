c42dbb59ccbaac366953f35e32dc8a3f445639a4
Daniel Zhou <danzhou1810@gmail.com>
2017-06-23 11:49:55 -0400

    Optimized get posts for emails

--- app/mailers/customer_service_mailer.rb
+++ app/mailers/customer_service_mailer.rb
@@ -6,16 +6,18 @@ class CustomerServiceMailer < ApplicationMailer
 	  	mail(:to => email, :subject => I18n.t("contact_us_email_subject"), :bcc => [Rails.application.config.email_list[:leon], Rails.application.config.email_list[:robin]])
 	end
 
-	def newsletter(email)
-		@news = Post.latest.news
-		@posts = Post.latest.posts
+	def newsletter(email, first_section, second_section)
+		@news = first_section
+		@posts = second_section
 		subject = @posts.first.title_en + ' | ' + @posts.first.title_cn
 		mail(:to => email, :subject => subject)
 	end
 
 	def self.send_newsletter(recipients)
+		news_posts = Post.latest.news
+		headline_posts = Post.latest.posts
 		recipients.each do |recipient|
-			newsletter(recipient.email).deliver
+			newsletter(recipient.email, news_posts, headline_posts).deliver
 		end
 	end
 
