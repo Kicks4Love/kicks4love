72cd1c74ee5f64b8a9acd2b2ade00a59ba09bc53
Liang-Hsuan Ma <leonma333@hotmail.com>
2017-06-23 15:47:29 -0400

    fix uninitialized constant migration file

--- db/migrate/20170614184235_create_subsribers.rb
+++ db/migrate/20170614184235_create_subsribers.rb
@@ -1,4 +1,4 @@
-class CreateSubscribers < ActiveRecord::Migration[5.0]
+class CreateSubsribers < ActiveRecord::Migration[5.0]
   	def change
     	create_table :subsribers do |t|
       		t.string :email
