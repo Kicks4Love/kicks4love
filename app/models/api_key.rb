a4e10d7c8dadc065f2a5017db9a1910b5e5dec67
Daniel Zhou <danzhou1810@gmail.com>
2017-07-26 15:09:57 -0400

    created ApiKey model

---
+++ app/models/api_key.rb
@@ -0,0 +1,13 @@
+class ApiKey < ApplicationRecord
+  belongs_to :admin_user
+  before_create :generate_access_token
+
+  private
+  
+  def generate_access_token
+    begin
+      self.access_token = SecureRandom.hex
+    end while self.class.exists?(access_token: access_token)
+  end
+
+end
